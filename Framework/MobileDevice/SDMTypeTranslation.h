d109034043ff195a2f8d7a1655089a9dc385363f
samdmarshall <me@samdmarshall.com>
2014-03-30 15:07:53 -0400

    adding AMD debugging macro

--- Framework/MobileDevice/SDMTypeTranslation.h
+++ Framework/MobileDevice/SDMTypeTranslation.h
@@ -7,10 +7,10 @@
 //  This file is made available under the LICENSE of the containing project
 //
 
-#import "SDMMobileDevice.h"
-
 #ifndef SDMMobileDevice_Framework_SDMTypeTranslation_h
 #define SDMMobileDevice_Framework_SDMTypeTranslation_h
+
+#include "SDMMobileDevice.h"
 
 #pragma mark - Errors
 
@@ -50,5 +50,20 @@ typedef SDMMD_AMRestorableDeviceRef AMRestorableDeviceRef;
 
 typedef SDMMD_AFCConnectionRef AFCConnectionRef;
 typedef SDMMD_AFCOperationRef AFCOperationRef;
+
+// arg_def should be formated as (ArgType1 arg1, ArgType2 arg2, ArgType3 arg3, etc)
+// arg_imp should be formated as (arg1, arg2, arg3, etc)
+#define AMD_INLINE(return_type, call, arg_def, arg_imp) static inline return_type call arg_def  { return SDMMD_##call arg_imp ; }
+
+AMD_INLINE(bool, AMDeviceIsValid, (AMDeviceRef device), (device))
+AMD_INLINE(AMDError, AMDeviceConnect, (AMDeviceRef device), (device))
+AMD_INLINE(AMDError, AMDeviceDisconnect, (AMDeviceRef device), (device))
+AMD_INLINE(AMDError, AMDeviceActivate, (AMDeviceRef device, CFDictionaryRef options), (device, options))
+AMD_INLINE(AMDError, AMDeviceDeactivate, (AMDeviceRef device), (device))
+AMD_INLINE(bool, AMDeviceIsPaired, (AMDeviceRef device), (device))
+AMD_INLINE(AMDError, AMDeviceValidatePairing, (AMDeviceRef device), (device))
+AMD_INLINE(AMDError, AMDevicePair, (AMDeviceRef device), (device))
+AMD_INLINE(AMDError, AMDevicePairWithOptions, (AMDeviceRef device, CFMutableDictionaryRef record), (device, record))
+AMD_INLINE(AMDError, AMDeviceUnpair, (AMDeviceRef device), (device))
 
 #endif
