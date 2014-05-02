cc97747125c8a6c97553814c74d692aff9f77b37
Jeremy Agostino <jeremyagost@gmail.com>
2014-05-02 16:03:29 -0400

    Missing newline

--- Framework/MobileDevice/SDMMDevice/SDMMD_AMDevice.c
+++ Framework/MobileDevice/SDMMDevice/SDMMD_AMDevice.c
@@ -409,7 +409,7 @@ CFTypeRef SDMMD_copy_lockdown_value(SDMMD_AMDeviceRef device, CFStringRef domain
 		result = kAMDInvalidArgumentError;
 	}
 	if (result != kAMDSuccess) {
-		printf("%s: Could not copy value (%x)", __FUNCTION__, result);
+		printf("%s: Could not copy value (%x)\n", __FUNCTION__, result);
 	}
 	return value;
 }
