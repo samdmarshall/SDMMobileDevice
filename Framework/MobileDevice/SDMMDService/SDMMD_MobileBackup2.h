e1c8cd12d605eb40b06dd6df26f3f38518d79757
samdmarshall <me@samdmarshall.com>
2014-05-03 18:09:54 -0400

    fixing typo

--- Framework/MobileDevice/SDMMDService/SDMMD_MobileBackup2.h
+++ Framework/MobileDevice/SDMMDService/SDMMD_MobileBackup2.h
@@ -44,7 +44,49 @@
  it seems that for doing a restore, you need to parse the mbdb
  
  
- 
+
+// for files that are not on the host system
+Received:
+<CFArray>=[
+           <CFStringRef>={DLMessageDownloadFiles}
+           <CFArray>=[
+                      <CFStringRef>={435f770a03b4ff48d7540f3d85851f845e6e14ac/Info.plist}
+                      ]
+           <CFDictionary>={
+           }
+           <CFNumberRef>=(kCFNumberFloat64Type){0.000000}
+           <CFNumberRef>=(kCFNumberSInt64Type){0}
+           ]
+
+
+If file doesn't exist...
+
+Send: (length + 0x06)
+0000 0001 06
+
+Send: (four zeros raw)
+0000 0000
+
+Send Message:
+<CFArray>=[
+           <CFStringRef>={DLMessageStatusResponse}
+           <CFNumberRef>=(kCFNumberSInt64Type){-13}	// magic error number :P
+           <CFStringRef>={Multi status}
+           <CFDictionary>={
+               <CFStringRef>={435f770a03b4ff48d7540f3d85851f845e6e14ac/Info.plist}: <CFDictionary>={
+                   <CFStringRef>={DLFileErrorString}: <CFStringRef>=Invalid object referenced in backup image	// this is just an example
+                   <CFStringRef>={DLFileErrorCode}: <CFNumberRef>=(kCFNumberSInt64Type){-6}	// this is the error code I've seen for file not found
+               }
+           }
+           ]
  */
+
+#include <CoreFoundation/CoreFoundation.h>
+#include "SDMMD_Error.h"
+#include "SDMMD_Connection.h"
+
+CFMutableArrayRef SDMMD_MB2StatusResponseMessage();
+sdmmd_return_t SDMMD_MB2SendFile(SDMMD_AMConnectionRef conn, CFStringRef path, CFDataRef file);
+sdmmd_return_t SDMMD_MB2SendFiles(SDMMD_AMConnectionRef conn, CFArrayRef paths, CFArrayRef files);
 
 #endif
